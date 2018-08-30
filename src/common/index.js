8fc252f7a3083e1e2016a52deafc583b4d75abdb
jw397 <jw397@126.com>
2018-08-30 18:36:05 +0800

    feat: 事件处理器放common里

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,9 @@
 import uinv from './uinv_util';
 import Q from './bluebird-ex';
 import {u} from './uinv';
+import eventEmitter from './event';
+
+uinv.eventEmitter = eventEmitter;
 
 export default uinv;
 export {u, Q};
