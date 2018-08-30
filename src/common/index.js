1b5408b481867805803c4fb7f3763833f504b131
jw397 <jw397@126.com>
2018-08-30 18:48:44 +0800

    perf: 事件还是分开，在node端用原生实现

--- src/common/index.js
+++ src/common/index.js
@@ -1,9 +1,6 @@
 import uinv from './config_tools';
 import Q from './bluebird-ex';
 import {u} from './uinv';
-import eventEmitter from './event';
-
-uinv.eventEmitter = eventEmitter;
 
 export default uinv;
 export {u, Q};
