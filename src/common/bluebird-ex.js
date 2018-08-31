e22a67e43f81985f2c83f7a30940cbbb46bb34c7
jw397 <jw397@126.com>
2018-08-31 18:58:00 +0800

    feat: then的处理会影响原生bluebird

--- src/common/bluebird-ex.js
+++ src/common/bluebird-ex.js
@@ -4,31 +4,8 @@
  * @date 2017-12-06
  */
 const Promise = require('bluebird');
-const clone = require('clone');
 
-var isFunction = function (fn) {
-  return Object.prototype.toString.call(fn) === '[object Function]';
-};
-
-//增加一个停止链式的方法
-var STOP_VALUE = {};//只要外界无法“===”这个对象就可以了
-var STOPPER_PROMISE = Promise.resolve(STOP_VALUE);
-
-Promise.prototype.__then__ = Promise.prototype.then;
-
-Promise.stop = function () {
-  return STOPPER_PROMISE;//不是每次返回一个新的Promise，可以节省内存
-};
-
-Promise.prototype.then = function (onResolved, onRejected) {
-  return this.__then__(function (value) {
-    return value === STOP_VALUE || (isFunction(onResolved) ? onResolved(value) : onResolved);
-  }, onRejected);
-};
-
-var Q = clone(Promise);
-
-Q.defer = function () {
+Promise.defer = function () {
   var _resolve, _reject;
   var promise = new Promise(function (resolve, reject) {
     _resolve = resolve;
@@ -41,8 +18,8 @@ Q.defer = function () {
   };
 };
 //只用来判断是否bluebird
-Q.isPromise = function (promise) {
+Promise.isPromise = function (promise) {
   return promise instanceof Promise;
 };
 
-export default Q;
+export default Promise;
