357ac43645c856643b36a654283efa9e0447432c
jw397 <jw397@126.com>
2019-04-25 21:23:50 +0800

    build: 直接一个webpack文件处理

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,8 +1,6 @@
-import {Q} from '../common';
 import uinv from './uinv';
 import util from './util';
 
 uinv.hash.combine(uinv.util, util);
 
-export {Q};
 export default uinv;
--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,4 @@
 import './polyfill';
-import Q from './Q';
 import uinv from './uinv';
 import util from './util';
 import hash from './hash';
@@ -8,4 +7,3 @@ uinv.util = util;
 uinv.hash = hash;
 
 export default uinv;
-export {Q};
