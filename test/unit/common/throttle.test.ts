d478c8fade19005169e72da43db94de1009ef14f
jw397 <jw397@126.com>
2019-04-28 18:46:34 +0800

    test(unit): throttle验证

--- test/unit/common/throttle.test.ts
+++ test/unit/common/throttle.test.ts
@@ -2,8 +2,19 @@ import {assert, expect} from 'chai';
 import throttle from '../../../src/common/throttle';
 
 describe('throttle 验证', function () {
-  it('暴露接口验证', function (done) {
+  it('暴露接口验证', function () {
     assert.isFunction(throttle);
+  });
+
+  it('直接执行', function () {
+    let num = 0;
+    const func = function () {
+      num++;
+    };
+    for (let i = 0; i < 100; i++) {
+      func();
+    }
+    assert.equal(num, 100);
   });
 
   it('延时验证', function (done) {
@@ -16,8 +27,10 @@ describe('throttle 验证', function () {
     }
     assert.equal(num, 0);
     setTimeout(function () {
-      assert.equal(num, 1);
+      assert.equal(num, 1, '过上一秒，函数只执行了一次');
       done();
     }, 1000);
   });
+
+
 });
