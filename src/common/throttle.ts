c7e954d9b0fff7d4d76864e17834529df516596e
jw397 <jw397@126.com>
2019-04-30 18:49:14 +0800

    unit(throttle): throttle添加单元测试

--- src/common/throttle.ts
+++ src/common/throttle.ts
@@ -3,18 +3,16 @@ const _now = function () {
 };
 
 interface ThrottleOpts {
-  trailing: boolean,
-  leading: boolean
+  leading: boolean, //如果想忽略开始边界上的调用则传入 {trailing:false},
+  trailing: boolean //如果想忽略结束边界上的调用则传入 {leading:false},
 }
 
 /**
  * 节流器
- *  options的默认值
- *  表示首次调用返回值方法时，会马上调用func；否则仅会记录当前时刻，当第二次调用的时间间隔超过wait时，才调用func。
- *  options.leading = true;
- * 表示当调用方法时，未到达wait指定的时间间隔，则启动计时器延迟调用func函数，若后续在既未达到wait指定的时间间隔和func函数又未被调用的情况下调用返回值方法，则被调用请求将被丢弃。
- *  options.trailing = true;
- * 注意：当options.trailing = false时，效果与上面的简单实现效果相同
+ * options的默认值
+ * options.leading = true; 表示首次调用返回值方法时，会马上调用func；否则仅会记录当前时刻，当第二次调用的时间间隔超过wait时，才调用func。
+ *
+ * options.trailing = true; 表示当调用方法时，未到达wait指定的时间间隔，则启动计时器延迟调用func函数，若后续在既未达到wait指定的时间间隔和func函数又未被调用的情况下调用返回值方法，则被调用请求将被丢弃。
  */
 const throttle = function (func: Function, wait: number = 300, options: Partial<ThrottleOpts> = {}) {
   let timeout, context, args, result;
@@ -43,7 +41,9 @@ const throttle = function (func: Function, wait: number = 300, options: Partial<
       }
       previous = now;
       result = func.apply(context, args);
-      if (!timeout) context = args = null;
+      if (!timeout) {
+        context = args = null;
+      }
     } else if (!timeout && options.trailing !== false) {
       // options.trailing=true时，延时执行func函数
       timeout = setTimeout(later, remaining);
@@ -62,8 +62,11 @@ const throttle = function (func: Function, wait: number = 300, options: Partial<
 
 /**
  * 防抖器
+ * 空闲控制函数， fn仅执行一次
+ * 如果想忽略开始边界上的调用则传入 {leading:false},
+ * 如果想忽略结束边界上的调用则传入 {trailing:false},
  */
-const debounce = function (func: Function, wait: number, immediate: boolean = false) {
+const debounce = function (func: Function, wait: number = 300, immediate: boolean = false) {
   // immediate默认为false
   let timeout, args, context, timestamp, result;
 
--- test/unit/common/throttle.test.ts
+++ test/unit/common/throttle.test.ts
@@ -1,5 +1,5 @@
 import {assert, expect} from 'chai';
-import throttle from '../../../src/common/throttle';
+import {throttle} from '../../../src/common/throttle';
 
 describe('功能 验证', function () {
   it('暴露接口验证', function () {
@@ -16,100 +16,75 @@ describe('功能 验证', function () {
     }
     assert.equal(num, 100);
   });
-
-  it('延时验证', function (done) {
-    let num = 0;
-    const func = function () {
-      num++;
-    };
-    for (let i = 0; i < 100; i++) {
-      throttle(func);
-    }
-    assert.equal(num, 0);
-    setTimeout(function () {
-      assert.equal(num, 1, '函数只执行了一次');
-      done();
-    }, 400);
-  });
 });
 
 describe('参数 验证', function () {
-  it('不同id', function (done) {
+  it('不传参数', function (done) {
     let num = 0;
     const func = function () {
       num++;
     };
+    const func2 = throttle(func);
+    assert.equal(num, 0, '开始不执行');
     for (let i = 0; i < 100; i++) {
-      throttle(func, {
-        id: i
-      });
+      func2();
     }
-    assert.equal(num, 0);
+    assert.equal(num, 1, '不传递参数，执行一次');
     setTimeout(function () {
-      assert.equal(num, 100, '不同的函数其实执行了100次');
-      done();
-    }, 400);
+        assert.equal(num, 1, '时间不到，还是只有1次');
+      },
+      200);
+    setTimeout(function () {
+        assert.equal(num, 2, '函数执行了2次，开始一次，结果一次');
+        done();
+      },
+      400);
+  });
+  it('参数为 leading=false', function (done) {
+    let num = 0;
+    const func = function () {
+      num++;
+    };
+    const func2 = throttle(func, 300, {leading: false});
+    assert.equal(num, 0, '开始不执行');
+    for (let i = 0; i < 100; i++) {
+      func2();
+    }
+    assert.equal(num, 0, '也不执行');
+    setTimeout(function () {
+        assert.equal(num, 0, '不到300秒，函数没有执行');
+      },
+      200);
+    setTimeout(function () {
+        assert.equal(num, 1, '函数执行了1次');
+        done();
+      },
+      400);
   });
 
-  it('不改变作用域', function (done) {
-    let name;
+  it('参数为 trailing=false', function (done) {
+    let num = 0;
     const func = function () {
-      if (this) {
-        name = this.name;
+      num++;
+    };
+    const func2 = throttle(func, 300, {trailing: false});
+    assert.equal(num, 0, '开始不执行');
+    for (let i = 0; i < 100; i++) {
+      func2();
+      if(i ==0){
+        assert.equal(num, 1, '结尾时会执行一次');
       }
-    };
-    throttle(func);
+    }
+    assert.equal(num, 1, '结尾时会执行一次');
     setTimeout(function () {
-      assert.isUndefined(name, '未定义');
-      done();
-    }, 400);
-  });
-
-  it('改变作用域', function (done) {
-    let name;
-    const func = function () {
-      name = this.name;
-    };
-    const obj = {
-      name: 'aa'
-    };
-    throttle(func, {
-      context: obj
-    });
+        assert.equal(num, 1, '函数执行了1次');
+      },
+      200);
     setTimeout(function () {
-      assert.equal(name, 'aa');
-      done();
-    }, 400);
+        assert.equal(num, 1, '函数执行了1次');
+        done();
+      },
+      400);
   });
-
-  it('传递参数', function (done) {
-    let name;
-    const func = function (params) {
-      name = params;
-    };
-    throttle(func, {
-      args: [123]
-    });
-    setTimeout(function () {
-      assert.equal(name, 123);
-      done();
-    }, 400);
-  });
-
-  // it('过期时间', function (done) {
-  //   let num =0;
-  //   const func = function () {
-  //     num ++;
-  //   };
-  //   for (let i = 0; i < 100; i++) {
-  //     throttle(func, {
-  //       time: 200
-  //     });
-  //   }
-  //   setTimeout(function () {
-  //     assert.equal(num, 123);
-  //     done();
-  //   }, 400);
-  // });
 
 });
