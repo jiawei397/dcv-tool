3b0ec1502d7779e18d53ce167ecfe76e5ab1c87f
jiawei397 <jwqd397>
2019-05-25 12:41:45 +0800

    feat: 加入版本号

--- src/common/index.ts
+++ src/common/index.ts
@@ -2,8 +2,9 @@ import './polyfill';
 import uinv from './uinv';
 import util from './util';
 import hash from './hash';
-
+import {version} from '../../package.json';
 uinv.util = util;
 uinv.hash = hash;
+uinv.VERSION = version;
 
 export default uinv;
