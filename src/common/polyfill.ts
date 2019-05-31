9606fa2688e1aaf25ed3c4723e37531519e03166
jw397 <jw397@126.com>
2019-05-31 15:04:05 +0800

    feat(array): 增加数组的扩展

--- src/common/polyfill.ts
+++ src/common/polyfill.ts
@@ -40,7 +40,7 @@ if (!String.prototype.hasOwnProperty('replaceAll')) {
 /**
  * 获取路径
  */
-String.prototype.getPath = function () {
+(String.prototype as any).getPath = function () {
   let startIndex = this.indexOf('\\') >= 0 ? this.lastIndexOf('\\') : this.lastIndexOf('/');
   return this.substring(0, startIndex);
 };
@@ -48,7 +48,7 @@ String.prototype.getPath = function () {
 /**
  * 获取扩展名
  */
-String.prototype.getExtension = function () {
+(String.prototype as any).getExtension = function () {
   let strings = this.split('.');
   if (strings.length > 1) {
     return strings.pop();
@@ -59,7 +59,7 @@ String.prototype.getExtension = function () {
 /**
  * 获取文件名
  */
-String.prototype.getFileName = function () {
+(String.prototype as any).getFileName = function () {
   let withExtension = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : true;
   let startIndex = this.indexOf('\\') >= 0 ? this.lastIndexOf('\\') : this.lastIndexOf('/');
 
@@ -84,24 +84,24 @@ String.prototype.getFileName = function () {
   }
 };
 
-String.prototype._trimLeft = function (charlist) {
-  if (charlist === undefined) charlist = "\s";
-  return this.replace(new RegExp("^[" + charlist + "]+"), "");
+(String.prototype as any)._trimLeft = function (charlist) {
+  if (charlist === undefined) charlist = '\s';
+  return this.replace(new RegExp('^[' + charlist + ']+'), '');
 };
 
-String.prototype._trimRight = function (charlist) {
-  if (charlist === undefined) charlist = "\s";
-  return this.replace(new RegExp("[" + charlist + "]+$"), "");
+(String.prototype as any)._trimRight = function (charlist) {
+  if (charlist === undefined) charlist = '\s';
+  return this.replace(new RegExp('[' + charlist + ']+$'), '');
 };
 
-String.prototype.trimBoth = function (charlist) {
+(String.prototype as any).trimBoth = function (charlist) {
   return this._trimLeft(charlist)._trimRight(charlist);
 };
 
 /**
  * 添加路径
  */
-String.prototype.appendPath = function (path) {
+(String.prototype as any).appendPath = function (path) {
   let parentPath = this._trimRight('/');
   return parentPath + '/' + path._trimLeft('/')._trimRight('/');
 };
@@ -216,38 +216,38 @@ if (!Array.prototype.fill) {
 
 if (!Array.from) {
   Array.from = (function () {
-    var toStr = Object.prototype.toString;
-    var isCallable = function (fn) {
+    let toStr = Object.prototype.toString;
+    let isCallable = function (fn) {
       return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
     };
-    var toInteger = function (value) {
-      var number = Number(value);
-      if (isNaN(number)) { return 0; }
-      if (number === 0 || !isFinite(number)) { return number; }
-      return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+    let toInteger = function (value) {
+      let num = Number(value);
+      if (isNaN(num)) { return 0; }
+      if (num === 0 || !isFinite(num)) { return num; }
+      return (num > 0 ? 1 : -1) * Math.floor(Math.abs(num));
     };
-    var maxSafeInteger = Math.pow(2, 53) - 1;
-    var toLength = function (value) {
-      var len = toInteger(value);
+    let maxSafeInteger = Math.pow(2, 53) - 1;
+    let toLength = function (value) {
+      let len = toInteger(value);
       return Math.min(Math.max(len, 0), maxSafeInteger);
     };
 
     // The length property of the from method is 1.
     return function from(arrayLike/*, mapFn, thisArg */) {
       // 1. Let C be the this value.
-      var C = this;
+      let C = this;
 
       // 2. Let items be ToObject(arrayLike).
-      var items = Object(arrayLike);
+      let items = Object(arrayLike);
 
       // 3. ReturnIfAbrupt(items).
       if (arrayLike == null) {
-        throw new TypeError("Array.from requires an array-like object - not null or undefined");
+        throw new TypeError('Array.from requires an array-like object - not null or undefined');
       }
 
       // 4. If mapfn is undefined, then let mapping be false.
-      var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
-      var T;
+      let mapFn = arguments.length > 1 ? arguments[1] : undefined;
+      let T;
       if (typeof mapFn !== 'undefined') {
         // 5. else
         // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
@@ -263,18 +263,18 @@ if (!Array.from) {
 
       // 10. Let lenValue be Get(items, "length").
       // 11. Let len be ToLength(lenValue).
-      var len = toLength(items.length);
+      let len = toLength(items.length);
 
       // 13. If IsConstructor(C) is true, then
       // 13. a. Let A be the result of calling the [[Construct]] internal method
       // of C with an argument list containing the single item len.
       // 14. a. Else, Let A be ArrayCreate(len).
-      var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+      let A = isCallable(C) ? Object(new C(len)) : new Array(len);
 
       // 16. Let k be 0.
-      var k = 0;
+      let k = 0;
       // 17. Repeat, while k < len… (also steps a - h)
-      var kValue;
+      let kValue;
       while (k < len) {
         kValue = items[k];
         if (mapFn) {
@@ -296,6 +296,96 @@ if (!Array.of) {
   Array.of = function() {
     return Array.prototype.slice.call(arguments);
   };
+}
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.findIndex
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      let o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      let len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      let thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      let k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        let kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return -1.
+      return -1;
+    }
+  });
+}
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      let o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      let len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      let thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      let k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        let kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
 }
 
 /**
